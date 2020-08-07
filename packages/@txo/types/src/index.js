51e9bcbe6aa5bfe606821aa2caba3e471c1d1b0e
Erik Slovak <erik.slovak@technologystudio.sk>
2020-08-07 09:48:52 +0200

    add castString helper function

---
+++ packages/@txo/types/src/index.js
@@ -0,0 +1,8 @@
+/**
+ * @Author: Rostislav Simonik <rostislav.simonik@technologystudio.sk>
+ * @Date: 2019-05-24T13:05:43+02:00
+ * @Copyright: Technology Studio
+ * @flow
+**/
+
+export * from './Api/CastUtils'
