29bb836b14597b5278e71afce4af68e22fabc125
Erik Slovak <erik.slovak@technologystudio.sk>
2023-08-15 18:58:24 +0200

    chore: add eslint disable and add file header

--- src/Api/Function.ts
+++ src/Api/Function.ts
@@ -1,3 +1,10 @@
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2023-08-15T18:08:23+02:00
+ * @Copyright: Technology Studio
+**/
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const isFunction = (value: any): value is (...args: any[]) => any => (
   typeof value === 'function'
 )
