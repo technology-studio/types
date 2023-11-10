f2614dce7e90566591be21ed33266daa214c20a1
Erik Slovak <erik.slovak@technologystudio.sk>
2023-11-10 14:00:54 +0100

    chore: add tsconfig for example files

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,14 @@ const txoConfig = require('eslint-config-txo-typescript')
 /** @type {import('eslint').Linter.FlatConfig[]} */
 const config = [
   ...txoConfig.default,
+  {
+    files: ['example/**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: './example/tsconfig.json'
+      }
+    }
+  }
 ]
 
 module.exports = config
