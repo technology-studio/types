f956812a3351d3e64fd6b1e8ceeedb76818290b4
Erik Slovák <34162967+erik-slovak@users.noreply.github.com>
2023-11-13 20:04:54 +0100

    chore: migrate to flat eslint config (#395)
    * feat: migrate to flat eslint config
    
    * chore: add tsconfig for example files
    
    * chore: add comma
    
    * chore: add missing commas
    
    ---------
    
    Co-authored-by: Rostislav Simonik <rostislav.simonik@technologystudio.sk>

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+const txoConfig = require('eslint-config-txo-typescript')
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+const config = [
+  ...txoConfig.default,
+  {
+    files: ['example/**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: './example/tsconfig.json'
+      },
+    },
+  },
+]
+
+module.exports = config
