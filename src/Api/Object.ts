d6e7d52f606ed544955f81c9b62d7d375303a4c3
Erik Slovák <erik.slovak@technologystudio.sk>
2025-02-06 09:42:57 +0100

    chore: update eslint setup and sync boilerplate changes (#579)
    * chore: update eslint setup and sync boilerplate changes
    
    * chore: update eslint ci rules

--- src/Api/Object.ts
+++ src/Api/Object.ts
@@ -13,8 +13,9 @@ export const isObject = (obj: unknown): obj is Record<string | number | symbol, 
 )
 
 export const isEmptyObject = <OBJECT>(obj?: OBJECT | null): obj is OBJECT => !!(
+  // eslint-disable-next-line @typescript-eslint/no-magic-numbers -- check if object has no keys
   obj != null && Object.keys(obj).length === 0
 )
 
-// eslint-disable-next-line @typescript-eslint/ban-types
+// eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion -- this function is a type caster
 export const atLeastOne = <TYPE extends object | null | undefined>(value: TYPE): AtLeastOneNilable<TYPE> => value as AtLeastOneNilable<TYPE>
